3942eed817927b29b333dd3f4714340bc9a27a3f
Kitae's Github <aqua0405@gmail.com>
2023-07-14 00:51:43 +0900

    Update TodoService.java

--- src/kr/inhatc/junit/mokito/todo/data/api/TodoService.java
+++ src/kr/inhatc/junit/mokito/todo/data/api/TodoService.java
@@ -2,10 +2,16 @@ package kr.inhatc.junit.mokito.todo.data.api;
 
 import java.util.List;
 
-// 1. Create TodoServiceStub
-// 2. Test TodoBusinessImpl using TodoServiceStub
+// 1. TodoServiceStub 만들기
+// 2. TodoServiceStub을 사용하여 TodoBusinessImpl 테스트하기
 public interface TodoService {
 	public List<String> retrieveTodos(String user);
 	
 	public List<String> configureSomething(String user);
+
+	/**
+	 * 삭제 메소드
+	 * @param todo
+	 */
+	public void deleteTodo(String todo);
 }
